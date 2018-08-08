b929e6b162399416599b787d928fbdab94ceace5
Ray Toal <rtoal@lmu.edu>
2018-08-08 10:42:22 -0700

    Some basic app functionality

---
+++ artist_service.py
@@ -0,0 +1,14 @@
+artist_info = {
+  'Taylor Swift': {
+      'image': 'taylor.png',
+      'spotify_link': '06HL4z0CvFAxyc27GXpf02',
+  },
+  'Christina Aguilera': {
+      'image': 'christina.jpg',
+      'spotify_link': '1l7ZsJRRS8wlW3WfJfPfNS',
+  },
+  'Abba': {
+      'image': 'abba.png',
+      'spotify_link': '0LcJLqbBmaGUft1e9Mm8HV'
+  },
+}
